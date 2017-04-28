d2844c52412cfb8611174700a48301030cdfc46f
rogercamps <rogercamps@me.com>
2017-04-28 14:50:14 -0700

    exercises on ryanstutorials: bash and scripting

---
+++ bash-scripting/exercise01.sh
@@ -0,0 +1,6 @@
+# Q:
+# A good place to start is to create a simple script which will accept some command line arguments and echo out some details about them 
+# A:
+echo "who are you"
+read whoIam
+echo "nice to meet you $whoIam" #!/usr/bin/env bash
